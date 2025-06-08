ae6713b48e7446fb3c5d3fd4461f19b38ac07004
Liam <liamcloss@gmail.com>
2025-06-08 20:05:45 +0100

    Fix terminology in comment

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -2,7 +2,7 @@
 # Write a programme where the computer randomly generates a number between 0
 # and 20. The user needs to guess what the number is. If the user guesses
 # wrong, tell them their guess is either too high, or too low. This will get
-# you started with the random library if you haven't already used it.
+# you started with the random module if you haven't already used it.
 import random
 def check_number(guess, answer, attempt):
     if guess < answer:
